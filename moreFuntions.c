51bc3a0c7f4f86f8e658b71a070abd1af8828a01
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-12-05 15:44:26 -0800

    Documentation the function printBinary

--- moreFuntions.c
+++ moreFuntions.c
@@ -1,5 +1,18 @@
+/*
+ *Authors: -Brayan Salazar
+ *	   -Camilo Fetecua
+ */
+
 #include "main.h"
 
+/**
+ * printBinary - Print the binary representation of an unsigned integer.
+ * @num: Unsigned integer to print in binary.
+ *
+ * This function prints the binary representation of the provided number.
+ *
+ * Return: The count of digits printed in the binary representation.
+ */
 int printBinary(unsigned int num) 
 {
 	char digit;
