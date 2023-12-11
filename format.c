ef97b5b6221311fe8fa187b62e026e3bf325445b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-12-10 17:39:06 -0800

    Fix format.c

--- format.c
+++ format.c
@@ -39,6 +39,9 @@ int formatSpecifier(char format, va_list arguments)
 	if (format == 'u')
 		return (printU(arguments));
 
+	if (format == 'r')
+		return (printRev(arguments));
+
 	if (format == 'R')
 		return (rot13(arguments));
 
